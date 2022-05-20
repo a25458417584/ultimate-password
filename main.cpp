ebf51c8268dd9adaf964b2914b89e856dd131848
a25458417584 <101554283+a25458417584@users.noreply.github.com>
2022-05-20 23:34:08 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <string>
+#include"Player.h"
 using namespace std;
 
 int main()
@@ -16,12 +17,11 @@ int main()
 	int max = 100, min = 0;
 
 	int guessNumber = rand() % 101;
-	int PlayerNumber = 0;
+	int PlayerNumber;
+	cout << "Enter your guess number(range:0~100): ";
+	cin >> PlayerNumber;
 	while (!(PlayerNumber == guessNumber))
 	{
-		cout << "Enter your guess number: ";
-		cin >> PlayerNumber;
-
 		if (PlayerNumber < min || PlayerNumber > max)
 		{
 			cout << "out of range, please Enter again" << endl;
@@ -30,25 +30,26 @@ int main()
 		{
 			if (PlayerNumber < guessNumber)
 			{
-				cout << "Not correct number in :";
 				min = PlayerNumber + 1;
-				cout << "not correct number , now number range in" << min << "~" << max << endl;
-				//player.plusGuessTimes();
+				cout << "not correct number , now number range in " << min << "~" << max << endl;
+				player.plusGuessTimes();
 			}
 
 			if (PlayerNumber > guessNumber)
 			{
-				cout << "Not correct number in :";
 				max = PlayerNumber - 1;
-				cout << "not correct number , now number range in" << min << "~" << max << endl;
-				//player.plusGuessTimes();
+				cout << "not correct number , now number range in " << min << "~" << max << endl;
+				player.plusGuessTimes();
 			}
 			if (PlayerNumber == guessNumber)
 			{
-				//player.plusGuessTimes();
+				player.plusGuessTimes();
 				break;
 			}
 		}
-		player.plusGuessTimes();
+
+		cout << "Enter your guess number: ";
+		cin >> PlayerNumber;
 	}
+	player.print() ;
 }
