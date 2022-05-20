1d1b333905c93428a9dccecbe8595076baa05484
j43992151 <101626855+j43992151@users.noreply.github.com>
2022-05-21 00:05:23 +0800

    Add files via upload

--- Player.h
+++ Player.h
@@ -1,1 +1,28 @@
 #pragma once
+#ifndef Player_H
+#define Player_H
+#include <iostream>
+using namespace std;
+
+class Player
+{
+public:
+	Player();
+	Player(string);
+	void setName(string);
+	void setGuessTimes(int);
+	string getName()const;
+	int getPoint()const;
+	void print();
+	void setPoint(int);
+	int getGuessTimes()const;
+	void setGuessTimes(int);
+	void plusGuessTimes();
+private:
+	string name;
+	int point;
+	int guessTimes;
+	
+
+};
+#endif
