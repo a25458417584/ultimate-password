593427b1e60d81a03fce467e85abd337b197adb7
yuenliu <101539991+yuenliu@users.noreply.github.com>
2022-05-20 23:54:51 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,48 +8,67 @@ using namespace std;
 int main()
 {
 	srand(time(0));
-
 	string name;
 	cout << "Enter your name: ";
 	cin >> name;
 	Player player(name, 0);
-
 	int max = 100, min = 0;
 
 	int guessNumber = rand() % 101;
+	int PlayerNumber = 0;
 	int PlayerNumber;
+	string isPlayAgain = "n";
 	cout << "Enter your guess number(range:0~100): ";
 	cin >> PlayerNumber;
-	while (!(PlayerNumber == guessNumber))
+	
+	while (isPlayAgain == "y")
 	{
-		if (PlayerNumber < min || PlayerNumber > max)
+		while (!(PlayerNumber == guessNumber))
 		{
-			cout << "out of range, please Enter again" << endl;
-		}
-		else
-		{
-			if (PlayerNumber < guessNumber)
-			{
-				min = PlayerNumber + 1;
-				cout << "not correct number , now number range in " << min << "~" << max << endl;
-				player.plusGuessTimes();
-			}
+			cout << "Enter your guess number: ";
+			cin >> PlayerNumber;
 
-			if (PlayerNumber > guessNumber)
+			if (PlayerNumber < min || PlayerNumber > max)
 			{
-				max = PlayerNumber - 1;
-				cout << "not correct number , now number range in " << min << "~" << max << endl;
-				player.plusGuessTimes();
+				cout << "out of range, please Enter again" << endl;
 			}
-			if (PlayerNumber == guessNumber)
+			else
 			{
-				player.plusGuessTimes();
-				break;
-			}
-		}
+				if (PlayerNumber < guessNumber)
+				{
+					cout << "Not correct number in :";
+					min = PlayerNumber + 1;
+					cout << "not correct number , now number range in" << min << "~" << max << endl;
+					//player.plusGuessTimes();
+					cout << "not correct number , now number range in " << min << "~" << max << endl;
+					player.plusGuessTimes();
+				}
 
-		cout << "Enter your guess number: ";
-		cin >> PlayerNumber;
+				if (PlayerNumber > guessNumber)
+				{
+					cout << "Not correct number in :";
+					max = PlayerNumber - 1;
+					cout << "not correct number , now number range in" << min << "~" << max << endl;
+					//player.plusGuessTimes();
+					cout << "not correct number , now number range in " << min << "~" << max << endl;
+					player.plusGuessTimes();
+				}
+				if (PlayerNumber == guessNumber)
+				{
+					//player.plusGuessTimes();
+					player.plusGuessTimes();
+					break;
+				}
+			}
+			player.plusGuessTimes();
+
+			cout << "Enter your guess number: ";
+			cin >> PlayerNumber;
+		}
+		cin >> isPlayAgainy;
+		if (isPlayAgain == "n")
+			break;
 	}
-	player.print() ;
+	
+	player.print();
 }
