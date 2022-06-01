7cd677aeb07f0b70a6e8e02f3c971d66afd7f614
j43992151 <101626855+j43992151@users.noreply.github.com>
2022-06-01 20:36:39 +0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -4,13 +4,12 @@ Player::Player()
 {
 	setName("John");
 	setPoint(0);
-	setGuessTimes(1);
 }
 Player::Player(string name)
 {
 	setName(name);
 	setPoint(0);
-	setGuessTimes(1);
+	setGuessTimes(0);
 }
 void Player::setName(string name)
 {
@@ -20,17 +19,9 @@ string Player::getName()const
 {
 	return name;
 }
-int Player::getPoint()const
-{
-	return point;
-}
 void Player::print()
 {
 	cout << "Player name:" << getName() << ", guessTimes:" << getGuessTimes() << endl;
-}
-void Player::setPoint(int point)
-{
-	this->point = point;
 }
 int Player::getGuessTimes()const
 {
